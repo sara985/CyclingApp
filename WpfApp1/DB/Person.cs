b92cf2f71e567ba532fbea5d13ab1695362f2dbb
EnnyMJE <78366762+EnnyMJE@users.noreply.github.com>
2022-08-01 17:44:08 +0200

    little adjustment in DB person

--- WpfApp1/DB/Person.cs
+++ WpfApp1/DB/Person.cs
@@ -7,8 +7,11 @@ namespace WpfApp1.DB
     {
         public string Mail { get; set; } = null!;
         public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
         public string Phone { get; set; } = null!;
         public string Password { get; set; } = null!;
-        public int Pos { get; set; }
+        public int Pos { get; set; } = 0!;
+        public string Address { get; set; } = null!;
+
     }
 }
--- WpfApp1/obj/Debug/net6.0-windows/Views/memberList.g.i.cs
+++ WpfApp1/obj/Debug/net6.0-windows/Views/memberList.g.i.cs
@@ -85,7 +85,7 @@ namespace WpfApp1.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp1;V1.0.0.0;component/views/memberlist.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp1;component/views/memberlist.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\memberList.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
