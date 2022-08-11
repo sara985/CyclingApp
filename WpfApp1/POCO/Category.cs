cdcfd7e9e7e3c8c231cad21db239e8ef1402628d
sara985 <61694349+sara985@users.noreply.github.com>
2022-08-11 17:55:39 +0200

    a few POCO classes added

---
+++ WpfApp1/POCO/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.POCO
+{
+    class Category
+    {
+        private int id;
+        private string name;
+    }
+}
