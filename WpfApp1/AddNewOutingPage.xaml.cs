3cbb5bc18e0e6d21f1d7fd03402be9d9e85f8f84
EnnyMJE <78366762+EnnyMJE@users.noreply.github.com>
2022-08-12 16:53:30 +0200

    Add new Outing from Manager interface

--- WpfApp1/AddNewOutingPage.xaml.cs
+++ WpfApp1/AddNewOutingPage.xaml.cs
@@ -23,5 +23,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
