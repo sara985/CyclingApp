647f4fd2018bb0013885b907feeb71db08d3d2af
sara985 <61694349+sara985@users.noreply.github.com>
2022-08-12 15:25:00 +0200

    member DAO created

---
+++ WpfApp1/DAO/MemberDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.POCO;
+
+namespace WpfApp1.DAO
+{
+    class MemberDAO : Interfaces.InterfaceDAO<Member>
+    {
+        public void Insert(Member t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Member t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Member t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
