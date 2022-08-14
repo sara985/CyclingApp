7202acdcadf6052fbf5ab8fd8a5871279cd888bf
EnnyMJE <78366762+EnnyMJE@users.noreply.github.com>
2022-08-14 13:19:25 +0200

    error message

--- WpfApp1/LoginPage.xaml.cs
+++ WpfApp1/LoginPage.xaml.cs
@@ -36,13 +36,14 @@ namespace WpfApp1
             }
             else
             {
-                this.Visibility = Visibility.Collapsed;
+                
                 
                 MemberDAO mem = new MemberDAO();
                 Member m = mem.GetByLogin(txtEmail.Text, txtPassword.Text);
 
                 if(m != null)
                 {
+                    this.Visibility = Visibility.Collapsed;
                     if (m.Position == 1)
                     {
                         TreasurerMainPage treasurerMain = new TreasurerMainPage();
@@ -59,11 +60,12 @@ namespace WpfApp1
                     }
                     
                 }
-
-
-
-
+                else
+                {
+                    MessageBox.Show("Email/password incorrect");
+                }
             }
+            
             
         }
 
