e50bca00b51f2aa2076711f440c33d37e096d815
sara985 <61694349+sara985@users.noreply.github.com>
2022-08-15 22:15:31 +0200

    outingDetail datacontext set

---
+++ WpfApp1/ViewModels/OutingDetailViewModel.cs
@@ -0,0 +1,26 @@
+﻿
+
+using WpfApp1.POCO;
+
+namespace WpfApp1.ViewModels
+{
+    class OutingDetailViewModel
+    {
+        private  Outing _outing;
+
+        public OutingDetailViewModel()
+        {
+        }
+
+        public OutingDetailViewModel(Outing outing)
+        {
+            _outing = outing;
+        }
+
+        public Outing Outing
+        {
+            get { return _outing; }
+            set { _outing = value; }
+        }
+    }
+}
